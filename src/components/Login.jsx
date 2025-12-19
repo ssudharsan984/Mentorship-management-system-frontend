1c554021ea972b398803f6cbfeefcc9f332795f1
ssudharsan984 <ssudharsan984@gmail.com>
2025-12-19 13:56:33 +0530

    Update Login.jsx

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -13,7 +13,7 @@ function Login() {
     setError("");
 
     try {
-      const res = await axios.post("http://localhost:5000/api/login", { username, password });
+      const res = await axios.post("https://mentorship-management-system-backend-1.onrender.com/api/login", { username, password });
 
       if (res.data.success) {
         localStorage.setItem("user", JSON.stringify(res.data));
